626b945a5c54bb571625b29428bee3511ec1fcde
FilipeUhlig <136847439+FilipeUhlig@users.noreply.github.com>
2023-06-23 15:53:44 -0300

    Add files via upload

---
+++ dicionario.py
@@ -0,0 +1,31 @@
+"""
+Programa: dicionario
+Descrição: Este programa apresenta a estrutura de dados "dicionário" em Python para fins de aprendizado.
+Autor: F
+Data : 23/06/2023
+Versão: 0.0.1
+"""
+
+#Atribuição de variáveis
+
+tabela = {"Arroz" : 0.5, "Batata" : 0.2, "Carne" : 0.1}
+
+
+#Entrada de dados
+
+
+
+
+#Processamento de dados
+
+
+
+
+
+#Saida de dadaos
+
+print(tabela["Arroz"])
+
+#print(tabela{"Alface"})
+
+#print(tabela["Alface"])
